aad6266686e382f97aa1217f330a77a41183de17
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-03-31 10:15:26 -0500

    cambios 2

--- src/main/java/com/empresa/controller/ModalidadController.java
+++ src/main/java/com/empresa/controller/ModalidadController.java
@@ -1,6 +1,7 @@
 package com.empresa.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,10 @@ public class ModalidadController {
 	@PostMapping
 	public ResponseEntity<?>  insertaModalidad(@RequestBody Modalidad obj){
 		List<String> lstSalida = new ArrayList<String>();
+		
+		obj.setEstado(1);
+		obj.setFechaRegistro(new Date());
+		obj.setFechaActualizacion(new Date());
 		
 		Modalidad objSalida = service.insertaModalidad(obj);
 		if (objSalida == null) {
